4bef800d11224c0d14490fb00018457b5be9926a
Kaiyi Wong <kaiwong1@uslaxssf3504.global.publicisgroupe.net>
2018-07-31 11:39:38 -0700

    update changes

---
+++ packages/react-scripts/template/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+  },
+  extends: ['react-app', 'airbnb'
+  ],
+  // Allows ESLint to resolve alias defined in react-scripts' webpack config
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: require.resolve(
+          '@kaiyiwong/react-scripts/config/webpack.config.dev.js'
+        ),
+      },
+    },
+  },
+  // add your custom rules here
+  rules: {
+    'jsx-a11y/href-no-hash': 'off', // This rule is used in CRA but has been removed in the later version of jsx-a11y used by airbnb
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+    // allow optionalDependencies
+    // 'import/no-extraneous-dependencies': ['error', {
+    //   'optionalDependencies': ['test/unit/index.js']
+    // }],
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': 'off',
+  },
+};
